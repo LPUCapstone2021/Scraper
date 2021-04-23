076e1a97c90194be64595e42fa43e3ff6e3bd195
Aniruddha Pandey <anirudh.pandev@gmail.com>
2021-04-24 00:56:11 +0530

    update zigwheels

--- Scraper/spiders/zigwheels.py
+++ Scraper/spiders/zigwheels.py
@@ -16,27 +16,26 @@ class ZigwheelsSpider(CrawlSpider):
 
     def parse_brand(self, response):
         for car_route in response.css('#modelList li .mk-img-h a::attr(href)').getall():
-            yield car_route
-            # url = response.urljoin(car_route)
-            # yield scrapy.Request(url, callback=self.parse_car_specs)
+            url = response.urljoin(car_route)
+            yield scrapy.Request(url, callback=self.parse_car_specs)
 
-    # def parse_car_specs(self, response):
-    #     loader = ItemLoader(item = ZigwheelsItem())
-    #     rating = {}
+    def parse_car_specs(self, response):
+        loader = ItemLoader(item = ZigwheelsItem())
+        rating = {}
 
-    #     for i, li in enumerate(response.css('ul.p-list li')):
-    #         if i > 2: break
-    #         rating[li.css('div.m-wl::text').get().lower()] = li.css('div.m-wr::text').get()
-    #     for li in response.css('div.rv-ls ul li'):
-    #         rating[li.css('div.m-wl::text').get().lower()] = li.css('div.m-wr::text').get()
+        for i, li in enumerate(response.css('ul.p-list li')):
+            if i > 2: break
+            rating[li.css('div.m-wl::text').get().lower()] = li.css('div.m-wr::text').get()
+        for li in response.css('div.rv-ls ul li'):
+            rating[li.css('div.m-wl::text').get().lower()] = li.css('div.m-wr::text').get()
 
-    #     loader.add_value('brand', response.url.split('/')[-2])
-    #     loader.add_value('name', response.url.split('/')[-1])
-    #     loader.add_value('mileage', rating['mileage'])
-    #     loader.add_value('comfort', rating['comfort'])
-    #     loader.add_value('performance', rating['performance'])
-    #     loader.add_value('maintenance', rating['maintenance'])
-    #     loader.add_value('safety', rating['safety'])
-    #     loader.add_value('features', rating['features'])
+        loader.add_value('brand', response.url.split('/')[-2])
+        loader.add_value('name', response.url.split('/')[-1])
+        loader.add_value('mileage', rating['mileage'])
+        loader.add_value('comfort', rating['comfort'])
+        loader.add_value('performance', rating['performance'])
+        loader.add_value('maintenance', rating['maintenance'])
+        loader.add_value('safety', rating['safety'])
+        loader.add_value('features', rating['features'])
 
-    #     yield loader.load_item()
+        yield loader.load_item()
